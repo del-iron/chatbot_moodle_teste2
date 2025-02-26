0e6f48250b65be1112d971a952dc14d35cf2bea5
Jardel Rocha <106354939+del-iron@users.noreply.github.com>
2025-02-26 11:41:38 -0300

    s

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_chatbot';  // Nome do plugin
+$plugin->version   = 2025022600;       // Versão (AAAA MM DD 00)
+$plugin->requires  = 2021051700;       // Requer Moodle 3.11+ (verificar sua versão)
+$plugin->maturity  = MATURITY_STABLE;  // Maturidade do plugin (STABLE, ALPHA, BETA)
+$plugin->release   = '1.0';            // Versão amigável
